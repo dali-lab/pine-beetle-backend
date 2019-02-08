eb706b4466a8e368874521be6460b0cfb82af566
Thomas Monfre <tmonfre@users.noreply.github.com>
2019-02-08 01:46:03 -0500

    if the user didn't specify a forest, run the model on each forest in the state, average the outputs, then return

--- src/router.js
+++ src/router.js
@@ -256,54 +256,139 @@ router.post('/getUniqueLocalForests', (req, res) => {
 
 router.post('/getPredictions', (req, res) => {
 	historical.getDataForPredictiveModel(req.body).then((data) => {
-		// initialize input counts
-		var SPB = 0;
-		var cleridst1 = 0;
-		var spotst1 = 0;
-		var spotst2 = 0;
-		var endobrev = 1;
+		// if the user selected a specific national forest or forest, simply run the model
+		if ((req.body.nf !== undefined && req.body.nf !== null && req.body.nf !== "") || (req.body.forest !== undefined && req.body.forest !== null && req.body.forest !== "")) {
+			// initialize input counts
+			var SPB = 0;
+			var cleridst1 = 0;
+			var spotst1 = 0;
+			var spotst2 = 0;
+			var endobrev = 1;
 
-		// sum up inputs across these filters
-		for (var entry in data) {
-			if (data[entry].year === parseInt(req.body.targetYear)) {
-				if (data[entry].spbPerTwoWeeks !== undefined) {
-					console.log("made it")
-					SPB += data[entry].spbPerTwoWeeks;
+			// sum up inputs across these filters
+			for (var entry in data) {
+				if (data[entry].year === parseInt(req.body.targetYear)) {
+					if (data[entry].spbPerTwoWeeks !== undefined) {
+						console.log("made it")
+						SPB += data[entry].spbPerTwoWeeks;
+					}
+				}
+				if (data[entry].year === parseInt(req.body.targetYear - 1)) {
+					if (data[entry].spots !== undefined) {
+						spotst1 += data[entry].spots;
+					}
+					if (data[entry].cleridsPerTwoWeeks !== undefined) {
+						cleridst1 += data[entry].cleridsPerTwoWeeks;
+					}
+				}
+				else if (data[entry].year === parseInt(req.body.targetYear - 2)) {
+					if (data[entry].spots !== undefined) {
+						spotst2 += data[entry].spots;
+					}
 				}
 			}
-			if (data[entry].year === parseInt(req.body.targetYear - 1)) {
-				if (data[entry].spots !== undefined) {
-					spotst1 += data[entry].spots;
-				}
-				if (data[entry].cleridsPerTwoWeeks !== undefined) {
-					cleridst1 += data[entry].cleridsPerTwoWeeks;
-				}
-			}
-			else if (data[entry].year === parseInt(req.body.targetYear - 2)) {
-				if (data[entry].spots !== undefined) {
-					spotst2 += data[entry].spots;
-				}
-			}
+
+			// make prediction
+			var results = makePredictions(SPB, cleridst1, spotst1, spotst2, endobrev);
+
+			// get results
+			var expSpotsIfOutbreak = results[2].Predictions;
+			var spots0 = results[3].Predictions;
+			var spots19 = results[4].Predictions;
+			var spots53 = results[5].Predictions;
+			var spots147 = results[6].Predictions;
+			var spots402 = results[7].Predictions;
+			var spots1095 = results[8].Predictions;
+
+			var predictions = [spots0, spots19, spots53, spots147, spots402, spots1095, expSpotsIfOutbreak]
+			var predPromise = Promise.resolve(predictions);
+
+			predPromise.then(function(value){
+			  res.send(value);
+			});
 		}
 
-		// make prediction
-		var results = makePredictions(SPB, cleridst1, spotst1, spotst2, endobrev);
+		// split the data up by forests, run the model each time, average the results
+		else {
+			// separate all data by forest
+			var forestsData = {}
 
-		// get results
-		var expSpotsIfOutbreak = results[2].Predictions;
-		var spots0 = results[3].Predictions;
-		var spots19 = results[4].Predictions;
-		var spots53 = results[5].Predictions;
-		var spots147 = results[6].Predictions;
-		var spots402 = results[7].Predictions;
-		var spots1095 = results[8].Predictions;
+			// sum up inputs across these filters
+			for (var entry in data) {
+				var forest = data[entry].forest;
+				let obj;
 
-		var predictions = [spots0, spots19, spots53, spots147, spots402, spots1095, expSpotsIfOutbreak]
-		var predPromise = Promise.resolve(predictions);
+				if (forest !== null && forest !== "") {
+					if (forestsData[forest] !== undefined) {
+						obj = forestsData[forest]
+					}
+					else {
+						obj = {
+							SPB: 0,
+							cleridst1: 0,
+							spotst1: 0,
+							spotst2: 0,
+							endobrev: 1
+						}
+					}
+				}
 
-		predPromise.then(function(value){
-		  res.send(value);
-		});
+				if (data[entry].year === parseInt(req.body.targetYear)) {
+					if (data[entry].spbPerTwoWeeks !== undefined) {
+						obj.SPB += data[entry].spbPerTwoWeeks;
+					}
+				}
+				if (data[entry].year === parseInt(req.body.targetYear - 1)) {
+					if (data[entry].spots !== undefined) {
+						obj.spotst1 += data[entry].spots;
+					}
+					if (data[entry].cleridsPerTwoWeeks !== undefined) {
+						obj.cleridst1 += data[entry].cleridsPerTwoWeeks;
+					}
+				}
+				else if (data[entry].year === parseInt(req.body.targetYear - 2)) {
+					if (data[entry].spots !== undefined) {
+						obj.spotst2 += data[entry].spots;
+					}
+				}
+
+				forestsData[forest] = obj;
+			}
+
+			// initialize a collection of sums
+			var sums = {
+				expSpotsIfOutbreak: 0,
+				spots0: 0,
+				spots19: 0,
+				spots53: 0,
+				spots147: 0,
+				spots402: 0,
+				spots1095: 0
+			}
+
+			// run model on each forest
+			for (var forest in forestsData) {
+				// make prediction
+				var results = makePredictions(forestsData[forest].SPB, forestsData[forest].cleridst1, forestsData[forest].spotst1, forestsData[forest].spotst2, forestsData[forest].endobrev);
+
+				// get results
+				sums.expSpotsIfOutbreak += results[2].Predictions;
+				sums.spots0 += results[3].Predictions;
+				sums.spots19 += results[4].Predictions;
+				sums.spots53 += results[5].Predictions;
+				sums.spots147 += results[6].Predictions;
+				sums.spots402 += results[7].Predictions;
+				sums.spots1095 += results[8].Predictions;
+			}
+
+			var numForests = Object.keys(forestsData).length;
+			var predictions = [sums.spots0 / numForests, sums.spots19 / numForests, sums.spots53 / numForests, sums.spots147 / numForests, sums.spots402 / numForests, sums.spots1095 / numForests, sums.expSpotsIfOutbreak / numForests]
+			var predPromise = Promise.resolve(predictions);
+
+			predPromise.then(function(value){
+			  res.send(value);
+			});
+		}
   	});
 });
 
