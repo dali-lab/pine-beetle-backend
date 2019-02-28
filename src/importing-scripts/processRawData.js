c5dcc20cfc4b27a9fce847993cb28b2f89f41e49
m-hess <madeline.e.hess.19@dartmouth.edu>
2019-02-27 20:40:31 -0500

    second pass, if statement set up

--- src/importing-scripts/processRawData.js
+++ src/importing-scripts/processRawData.js
@@ -8,7 +8,7 @@ const formatToSpot = (data) => {
   // var counties = []; //tracking, {county, state}
   // var traps = []; //error checking
   var countyTrapTotals = []; //{county, state, countTraps}
-  var countyTrapSeen = []; //{county, countTrapsSeen}
+  var countyTrapSeen = []; //{county, state, countTrapsSeen}
 
   var formattedSpotArray = [];
 
@@ -43,10 +43,51 @@ const formatToSpot = (data) => {
   }
 
   //Fill spots
+  //For each data point
   for (var i = 0; i < data.length; i++) {
     var observation = data[i];
+    var found2 = false;
+
+    //Find spot that matches county-state
+    for (var j = 0; j < formattedSpotArray.length; j++) {
+      var spot = formattedSpotArray[j];
+      if((spot.county === observation.County) && (spot.state === observation.USA_State)) {
+
+        for (var k = 0; k < countyTrapSeen.length; k++) {
+          //if we have seen a trap from this county-state before...
+          if ((countyTrapSeen[k][0] === spot.county) && (countyTrapSeen[k][1] === spot.state) {
+            found2 = true;
+
+            //if we are in the last round of traps for the county-state, then divde by max denominator
+            //maybe put a console log here
+            //if we are not in the last round of traps for the county-state, do nothing
+
+            countyTrapTotals[j][2]++;
+            break;
+          }
+          //if we haven't seen a trap from this county-state before...
+          else {
+            if (found2 === false) {
+              //do stuff...
+              //add to array
+              //do prelim calculations
+
+            }
+          }
+        }
+      }
+      //else spot doesn't match so do nothing
+    }
   }
 
+  //Error checking by testing to see if sum of trap counts by county-states = data.length
+  var count = 0;
+  for (var i = 0; i<countyTrapTotals.length; i++) {
+    count = count + countyTrapTotals[i][2];
+  }
+
+  return (count === data.length); //should return true
+  // return formattedSpotArray;
 }
 
 const process = { formatToSpot }
