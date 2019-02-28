a309d6c43caf1de395c5279d194a5d02eb0804dc
m-hess <madeline.e.hess.19@dartmouth.edu>
2019-02-27 19:52:33 -0500

    second pass, correct trap county compression tracking info for in case county names repeated

--- src/importing-scripts/processRawData.js
+++ src/importing-scripts/processRawData.js
@@ -5,30 +5,38 @@ import Trapping from '../models/trapping';
 //Use to process array of data in schema spots_v2 format
 const formatToSpot = (data) => {
   // WRAP: if (year == current year) otherwise need to implement year tracking
-  var counties = []; //tracking
-  var traps = []; //error checking
-  var countyTrapTotals = []; //{county, countTraps}
+  // var counties = []; //tracking, {county, state}
+  // var traps = []; //error checking
+  var countyTrapTotals = []; //{county, state, countTraps}
   var countyTrapSeen = []; //{county, countTrapsSeen}
+  var found = false;
 
   //Generate countryTrapTotals
   for (var i = 0; i<data.length; i++) {
     observation = data[i];
     //County
     for (var j = 0; j<countyTrapTotals.length; j++) {
-      //if county doesn't exist in array...
-      if (!counties.includes(observation.County)) {
-        counties.push(observation.County); //tracking
-        countyTrapTotals.push({observation.County, 1});
-      }
-      //if county does exist in array...
-      else {
-        replaceIndex = countyTrapTotals.findIndex(observation.County);
-        countyTrapTotals[replaceIndex][1]++; //increment by 1
+      //if {county, state} DOES exist in array...
+      if ((countyTrapTotals[j][0] === observation.County) && (countyTrapTotals[j][1] === observation.USA_State)) {
+          found = true;
+          countyTrapTotals[j][2]++;
+          break;
+      } else {
+        if (found === true) { //if we haven't already found {county, state}
+          var addMe = {observation.County, observation.USA_State, 1};
+          countyTrapTotals.push(addMe);
+        }
       }
     }
   }
 
   //Generate empty spots
+  for (var i = 0; i<countyTrapTotals.length; i++) {
+    const newProcessedSpot = {};
+    newProcessedSpot.county = observation.County; //need to map these to forests
+    newProcessedSpot.state = observation.USA_State;
+
+  }
 
   //Fill spots
 
