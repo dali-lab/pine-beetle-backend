da69f71f9e0a21c9843c670782b0b02ca10d9788
Thomas Monfre <tmonfre@users.noreply.github.com>
2019-03-03 20:01:21 -0500

    getPredictions is up to date

--- src/router.js
+++ src/router.js
@@ -322,14 +322,17 @@ router.post('/getPredictions', (req, res) => {
 					cleridst1: 0,
 					spotst1: 0,
 					spotst2: 0,
+					endobrev: req.body.endobrev,
 					stateCode: 0,
-					forestCode: 0
+					forestCode: 0,
+					forest: ""
 				}
 
 				// sum up inputs across these filters
 				for (var entry in data) {
-					modelInputs.stateCode = data[entry].stateCode
-					modelInputs.forestCode = data[entry].forestCode
+					modelInputs.stateCode = data[entry].stateCode;
+					modelInputs.forestCode = data[entry].forestCode;
+					modelInputs.forest = data[entry].forest;
 
 					if (data[entry].year === parseInt(req.body.targetYear)) {
 						if (data[entry].spbPerTwoWeeks !== undefined) {
@@ -352,8 +355,8 @@ router.post('/getPredictions', (req, res) => {
 				}
 
 				// make prediction
-				var results = makePredictions(modelInputs.SPB, modelInputs.cleridst1, modelInputs.spotst1, modelInputs.spotst2, 1);
-
+				var results = makePredictions(modelInputs.SPB, modelInputs.cleridst1, modelInputs.spotst1, modelInputs.spotst2, modelInputs.endobrev);
+	
 				// get results
 				var expSpotsIfOutbreak = results[2].Predictions;
 				var spots0 = results[3].Predictions;
@@ -362,10 +365,10 @@ router.post('/getPredictions', (req, res) => {
 				var spots147 = results[6].Predictions;
 				var spots402 = results[7].Predictions;
 				var spots1095 = results[8].Predictions;
-
+	
 				var predictions = [spots0, spots19, spots53, spots147, spots402, spots1095, expSpotsIfOutbreak]
 				var predPromise = Promise.resolve(predictions);
-
+	
 				predPromise.then(function(value){
 					// put model outputs into JSON object
 					var outputs = {
@@ -384,17 +387,17 @@ router.post('/getPredictions', (req, res) => {
 					});
 				});
 			}
-
+	
 			// split the data up by forests, determine representative forests of the sample, run the model on only representative forests
 			else {
 				// separate all data by forest
 				var forestsData = {}
-
+	
 				// sum up inputs across these filters
 				for (var entry in data) {
 					var forestNames = [data[entry].forest, data[entry].nf]
 					var obj = [null,null];
-
+	
 					// repeat for forest and national forest
 					for (var i=0; i < obj.length; i++) {
 						// if there is a forest to grab, find object, otherwise create one
@@ -408,11 +411,11 @@ router.post('/getPredictions', (req, res) => {
 									cleridst1: 0,
 									spotst1: 0,
 									spotst2: 0,
-									endobrev: 1
+									endobrev: req.body.endobrev
 								}
 							}
 						}
-
+	
 						// if we have an object to grab, get statistics for spb, spots, etc.
 						if (obj[i] !== null) {
 							if (data[entry].year === parseInt(req.body.targetYear)) {
@@ -433,30 +436,30 @@ router.post('/getPredictions', (req, res) => {
 									obj[i].spotst2 += data[entry].spots;
 								}
 							}
-
+				
 							forestsData[forestNames[i]] = obj[i];
 						}
 					}
 				}
-
+	
 				// grab array of spots values
 				var spots = []
 				for (var forest in forestsData) {
 					spots.push(forestsData[forest].spotst1)
 				}
-
+	
 				// compute mean and standard deviation of observed spots
 				var meanSpots = math.mean(spots);
 				var sdSpots = math.std(spots);
-
+	
 				// get count of number of forests chosen then determine number of forests to run the model on
 				var numForests = Object.keys(forestsData).length;
 				var numForestsForModel = parseInt(numForests / 4);
-
+	
 				// construct object for forests to run the model on
 				var forestsDataForModel = {}
 				var forestsAdded = 0;
-
+	
 				// get representative forests to run the model on
 				for (var forest in forestsData) {
 					if (forestsAdded < numForestsForModel) {
@@ -468,7 +471,7 @@ router.post('/getPredictions', (req, res) => {
 						}
 					}
 				}
-
+	
 				// initialize a collection of sums
 				var sums = {
 					expSpotsIfOutbreak: 0,
@@ -486,7 +489,7 @@ router.post('/getPredictions', (req, res) => {
 					spotst1: 0,
 					spotst2: 0
 				}
-
+	
 				// run model on each representative forest
 				for (var forest in forestsDataForModel) {
 					// add to outputSums
@@ -497,7 +500,7 @@ router.post('/getPredictions', (req, res) => {
 
 					// make prediction
 					var results = makePredictions(forestsDataForModel[forest].SPB, forestsDataForModel[forest].cleridst1, forestsDataForModel[forest].spotst1, forestsDataForModel[forest].spotst2, forestsDataForModel[forest].endobrev);
-
+	
 					// get results
 					sums.expSpotsIfOutbreak += results[2].Predictions;
 					sums.spots0 += results[3].Predictions;
@@ -507,7 +510,7 @@ router.post('/getPredictions', (req, res) => {
 					sums.spots402 += results[7].Predictions;
 					sums.spots1095 += results[8].Predictions;
 				}
-
+				
 				// get average model inputs
 				var averageModelInputs = {
 					SPB: outputSums.SPB / numForestsForModel,
@@ -515,10 +518,10 @@ router.post('/getPredictions', (req, res) => {
 					spotst1: outputSums.spotst1 / numForestsForModel,
 					spotst2: outputSums.spotst2 / numForestsForModel
 				}
-
+	
 				var predictions = [sums.spots0 / numForestsForModel, sums.spots19 / numForestsForModel, sums.spots53 / numForestsForModel, sums.spots147 / numForestsForModel, sums.spots402 / numForestsForModel, sums.spots1095 / numForestsForModel, sums.expSpotsIfOutbreak / numForestsForModel]
 				var predPromise = Promise.resolve(predictions);
-
+	
 				predPromise.then(function(value){
 					// put model outputs into JSON object
 					var outputs = {
@@ -548,7 +551,7 @@ router.post('/getCustomPredictions', (req, res) => {
 	var cleridst1 = parseInt(req.body.cleridst1);
 	var spotst1 = parseInt(req.body.spotst1);
 	var spotst2 = parseInt(req.body.spotst2);
-	var endobrev = 1;
+	var endobrev = parseInt(req.body.endobrev);
 
 	// make prediction
 	var results = makePredictions(SPB, cleridst1, spotst1, spotst2, endobrev);
