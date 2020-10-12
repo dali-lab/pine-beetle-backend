4b971b7cd75bfdc241246feacec9d2e7275e0310
Thomas Monfre <tmonfre1@gmail.com>
2020-10-12 18:19:47 -0400

    feat: add routers for prediction collections

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,4 +1,6 @@
+import countyPredictions from './county-predictions';
 import healthcheck from './healthcheck';
+import rangerDistrictPredictions from './ranger-district-predictions';
 import spots from './spots';
 import summarizedCounty from './summarized-county';
 import summarizedRangerDistrict from './summarized-ranger-district';
@@ -6,7 +8,9 @@ import unsummarized from './unsummarized';
 import user from './user';
 
 export default {
+  'county-prediction': countyPredictions,
   healthcheck,
+  'rd-prediction': rangerDistrictPredictions,
   'spot-data': spots,
   'summarized-county-trapping': summarizedCounty,
   'summarized-rangerdistrict-trapping': summarizedRangerDistrict,
