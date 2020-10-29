18e647e9c9f9ad7a68a3499cb43778b7b5aa6d8f
Thomas Monfre <tmonfre1@gmail.com>
2020-10-29 13:35:58 -0400

    chore: delete archive directory

--- archive/importing-scripts/uploadSurvey123toMongo.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const getData = async (token) => {
-  const survey123URL = `https://services3.arcgis.com/dOOr6AQOcTIg6tBw/arcgis/rest/services/service_e946164787a44d1b973f43cc96d889a3/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=${token}`;
-  return axios.get(survey123URL);
-};
-
-const upload = { getData };
-export default upload;
